20ec99aa8c28e5b140609050959ce023601892c1
Jferpon <jferpon089@g.educaand.es>
2025-05-26 09:15:20 +0200

    Correccion carpetas

---
+++ tareafinalJosue/src/main/java/controladores/ejemplo.java
@@ -0,0 +1,7 @@
+package main.java.controladores;
+
+public class ejemplo {
+    public static void main(String[] args) {
+        System.out.println("hola");
+    }
+}
---
+++ tareafinalJosue/src/main/java/entidades/EjemploEntidades.java
@@ -0,0 +1,7 @@
+package main.java.entidades;
+
+public class EjemploEntidades {
+    public static void main(String[] args) {
+        System.out.println("adios");
+    }
+}
